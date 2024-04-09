980279d1a64122672ab3796ff31e1c335033d021
hungvy <hung0001@stud.kea.dk>
2024-04-09 13:19:45 +0200

    Initial commit

---
+++ src/main/java/com/example/onskeskyen/OnskeskyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.onskeskyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnskeskyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnskeskyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onskeskyen/OnskeskyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onskeskyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnskeskyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
