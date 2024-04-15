f0d37484a9f3213de739f89720dba84ee14dbc69
olni0002 <145747740+olni0002@users.noreply.github.com>
2024-04-15 10:18:18 +0200

    Add files via upload

--- src/main/java/com/example/onskeskyen/models/Product.java
+++ src/main/java/com/example/onskeskyen/models/Product.java
@@ -1,9 +1,72 @@
-package com.example.onskeskyen.models;
+package com.example.onskeskyen.product;
 
 public class Product {
-    private int id;
+    private int product_id;
     private String name;
     private String description;
-    private int price;
+    private double price;
+    private int amount;
+
+    public Product() {
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getImagepath() {
+        return imagepath;
+    }
+
+    public void setImagepath(String imagepath) {
+        this.imagepath = imagepath;
+    }
+
+    public int getWishlist_id() {
+        return wishlist_id;
+    }
+
+    public void setWishlist_id(int wishlist_id) {
+        this.wishlist_id = wishlist_id;
+    }
+
+    private String imagepath;
+    private int wishlist_id;
 
 }
